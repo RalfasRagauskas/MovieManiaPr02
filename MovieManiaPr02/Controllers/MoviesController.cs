0eafd2ba8c585c9f1e2f2575172724cccb410ea9
Ralfas Ragauskas <Ralfasr02@gmail.com>
2025-12-08 19:59:21 +0100

    Add project files.

---
+++ MovieManiaPr02/Controllers/MoviesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieManiaPr02.Models;
+using MovieManiaPr02.Persistence;
+
+namespace MovieManiaPr02.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Index()
+        {
+            var movies = MovieRepository.GetAll();
+            return View(movies);
+        }
+    }
+}
---
+++ MovieManiaPr02/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿namespace MovieManiaPr02.Models
+{
+    public class Movie
+    {
+
+        public int MovieId { get; set; } 
+
+        public string Title { get; set; } = string.Empty;
+
+        public int ReleaseYear { get; set; }
+
+        public int DurationInMinutes { get; set; }
+
+        public string? Description { get; set; }
+
+        public int? Rating { get; set; }
+
+    }
+}
